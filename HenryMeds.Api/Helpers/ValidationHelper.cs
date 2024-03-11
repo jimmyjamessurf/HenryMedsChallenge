97c73a1fc2f388a8e72df62ac4a4fd4ddc63ca7a
James Novotny <james.novotny@gmail.com>
2024-03-11 14:18:24 -0500

    ?

--- HenryMeds.Api/Helpers/ValidationHelper.cs
+++ HenryMeds.Api/Helpers/ValidationHelper.cs
@@ -4,7 +4,7 @@ namespace HenryMeds.Api.Helpers
 {
     public static class ValidationHelper
     {
-        public static (bool IsValid, IEnumerable<string> Errors) ValidateModel<TModel>(TModel model)
+        public static (bool IsValid, IEnumerable<string?> Errors) ValidateModel<TModel>(TModel model)
         {
             var validationResults = new List<ValidationResult>();
             var validationContext = new ValidationContext(model);
